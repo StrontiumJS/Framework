03b7d6fb4c3918cec0f424bc2c12949b611cf457
Alexander Christie <alex@briefly.io>
2017-12-17 02:23:05 +0000

    Add comment to filter

--- src/framework/data/Filter.ts
+++ src/framework/data/Filter.ts
@@ -1,3 +1,9 @@
+/**
+ * A Filter represents a collection of statements that provide criteria for selecting data from
+ * a set.
+ *
+ * It is designed as an abstraction over a variety of different datastores query language.
+ */
 export type Filter = Array<
     [string, string, any] | [string, string, any] | "OR" | "AND"
 >
