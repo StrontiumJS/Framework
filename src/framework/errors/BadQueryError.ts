7ee7866560be91eb5570e571149ad3a4cad02d79
Alexander Christie <alexanderchristie@outlook.com>
2017-12-22 14:17:30 +0000

    Make the BadQueryError more assertive around error responsibility.

--- src/framework/errors/BadQueryError.ts
+++ src/framework/errors/BadQueryError.ts
@@ -1,6 +1,6 @@
 /**
  * A BadQueryError represents a logical error in the construction or execution of a query where the
- * responsibility for the failure normally rests on the programmer.
+ * responsibility for the failure rests on the programmer.
  *
  * This can represent either an error in the framework query layer or an error in the underlying
  * execution of the datastore.
