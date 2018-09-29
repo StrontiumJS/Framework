cd1bc78ee7f0b73b5eca128eaa8072653fefa4b7
Alexander Christie <alexander@fundstack.com>
2018-09-29 23:52:30 +0100

    Cleanup V1

--- src/framework/data/Filter.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A Filter represents a collection of statements that provide criteria for selecting data from
- * a set.
- *
- * It is designed as an abstraction over a variety of different datastores query language.
- */
-export type Filter<T> = Array<[keyof T, string, any] | "OR" | "AND">
