cc96ff9327ba0a84900b942fce2c41f835ad34d3
Alexander Christie <alexander@fundstack.com>
2018-10-07 21:56:15 +0100

    Refactor the compact method

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -1,17 +1,6 @@
-export type Func<I, O> = (input: I) => O
-export type Predicate<T> = Func<T, boolean>
-export type TypeGuard<T, V extends T> = (val: T) => val is V
-
-export function filter<T, S extends T>(arr: T[], func: TypeGuard<T, S>): S[]
-export function filter<T>(arr: T[], func: Predicate<T>): T[]
-export function filter<T>(arr: T[], func: Predicate<T>): T[] {
-    return arr.filter(func)
-}
-
-export function compact<T>(input: (T | null | undefined)[]): T[] {
-    return filter(
-        input,
-        (item: T | null | undefined): item is T =>
+export const compact = <T>(input: T[]): Exclude<T, null | undefined>[] => {
+    return input.filter(
+        (item: T): item is Exclude<T, null | undefined> =>
             item !== undefined && item !== null
     )
 }
