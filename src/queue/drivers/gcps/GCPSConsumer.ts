7d96eb174f188a999089e944b066e9d962070941
mfbx9da4 <dalberto.adler@gmail.com>
2018-12-18 15:31:57 +0000

    Update

--- src/queue/drivers/gcps/GCPSConsumer.ts
+++ src/queue/drivers/gcps/GCPSConsumer.ts
@@ -95,14 +95,13 @@ export class GCPSConsumer implements Process {
             await Promise.all(
                 messages.map(
                     async (m): Promise<void> => {
-                        let eventName = m.message.attributes
-                            ? m.message.attributes.STRONTIUM_EVENT_NAME
-                            : ""
                         try {
                             return this.executeTask(
                                 m.ackId,
                                 {
-                                    eventName,
+                                    eventName:
+                                        m.message.attributes
+                                            .STRONTIUM_EVENT_NAME,
                                     message: m.message.data,
                                 },
                                 container
