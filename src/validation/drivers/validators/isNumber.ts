9570b7816d5fc30a22ae270cb9eb2e00212f761d
Alexander Christie <alexander@fundstack.com>
2019-02-01 13:27:06 +0000

    Implement string casting in isNumber

--- src/validation/drivers/validators/isNumber.ts
+++ src/validation/drivers/validators/isNumber.ts
@@ -1,8 +1,16 @@
 import { ValidationError } from "../../../errors/http/ValidationError"
 
 export const isNumber = (input?: unknown): number => {
-    if (typeof input === "number") {
+    if (typeof input === "number" && !isNaN(input)) {
         return input
+    }
+
+    if (typeof input === "string") {
+        let parsedNumber = Number(input)
+
+        if (!isNaN(parsedNumber)) {
+            return parsedNumber
+        }
     }
 
     throw new ValidationError(
